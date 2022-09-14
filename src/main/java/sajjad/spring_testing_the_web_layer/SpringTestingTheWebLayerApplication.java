f816b5abfc6c9a40961996adc7134db71bc4141e
SajjadSedaqat <Sajjad.Sedaqat@Gmail.com>
2022-09-14 15:45:39 +0430

    Initial commit

---
+++ src/main/java/sajjad/spring_testing_the_web_layer/SpringTestingTheWebLayerApplication.java
@@ -0,0 +1,13 @@
+package sajjad.spring_testing_the_web_layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingTheWebLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestingTheWebLayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sajjad/spring_testing_the_web_layer/SpringTestingTheWebLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package sajjad.spring_testing_the_web_layer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingTheWebLayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
